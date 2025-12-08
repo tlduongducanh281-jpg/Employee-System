e7032312502e85b7a4bf840dd1f0d42a426b0b92
tlduongducanh281-jpg <tlduongducanh281@gmail.com>
2025-12-08 20:22:10 +0700

    Delete department (1).py

--- department (1).py
+++
@@ -1,9 +0,0 @@
-# Department.py
-
-class Department:
-    def __init__(self, department_id, department_name):
-        self.department_id = department_id
-        self.department_name = department_name
-
-    def __repr__(self):
-        return f"Department({self.department_id}, {self.department_name})"
